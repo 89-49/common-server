031b08a5510d4593f4f3e39a3265f082b487279a
jin <yjj1544@gmail.com>
2026-04-24 16:08:54 +0900

    refactor: 중복 빈 등록 방지를 위해 CustomAccessDeniedHandler의 Component 애너테이션 제거

--- src/main/java/org/pgsg/config/security/CustomAccessDeniedHandler.java
+++ src/main/java/org/pgsg/config/security/CustomAccessDeniedHandler.java
@@ -21,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Component
 @RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
