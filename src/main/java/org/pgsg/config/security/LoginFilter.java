3774524924c70e43968534d0bc7b7232593575cb
Hyeonbin2379 <hyeonbin2379@gmail.com>
2026-04-26 23:30:48 +0900

    [Fix+Chore] 공통 모듈 세부 사항 수정 및 패키지 배포 환경 세팅 (#10)
    * fix: SecurityUtil 컴파일 에러 발생 코드 수정
    
    - UnauthorizedException 예외 클래스가 없어 컴파일 에러가 발생했던 부분을 CustomException으로 대체하여 해결
    
    * refactor: LoginFilter, FeignClient 요청 헤더 필드 구성 수정
    
    - UserDetailsImpl을 기준으로 LoginFilter, FeignClient의 커스텀 요청 헤더 관련 필드 구성 수정
    
    * chore: 공통모듈 패키지 배포 환경 세팅
    
    - src/main/resources/META_INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports 파일 추가(AppCtx 클래스 내부의 모든 설정을 자동 적용)
    - build.gradle 파일 내 publishing 블록 추가
    - CommonApplicationTests의 @SpringBootTest 어노테이션을 주석처리하여 통합테스트 설정 비활성화
    (라이브러리 용도이므로 contextLoads() 테스트 불필요)
    
    * docs: 공통모듈 README 추가
    
    - 공통모듈에서 제공하는 기능 및 각 프로젝트에서 공통모듈을 build.gradle에 추가하여 사용하기 위한 방법에 관한 안내문 작성
    
    * fix: META-INF 오탈자 수정
    
    - META_INF의 폴더명을 META-INF로 수정
    
    * fix: 401 예외 처리 관련 오탈자 수정
    
    - UnAuthorizedException으로 적힌 부분을 UnauthorizedException으로 수정하여 예외 키 불일치로 401 대신 500으로 처리될 수 있다는 코드래빗 피드백 반영
    
    * fix: UserDetailsImpl 필드 구성 수정
    
    - nickname 필드 추가
    
    * refactor: FeignConfig, LoginFilter 필드 구성 수정
    
    - UserDetailsImpl의 필드에 nickname이 추가됨에 따라, 이를 반영하여 FeignConfig, LoginFilter의 필드 구성을 수정
    
    * fix: 코드래빗 수정사항 반영
    
    - UserDetailsImpl의 toString() 호출 시 사용자 닉네임 노출 방지
    - 사용자 인증 정보에 닉네임도 저장하도록 LoginFilter 내 UserDetailsImpl 빌더 관련 코드 수정
    
    * chore: build.gradle 의존성 버전 수정
    
    - 공통모듈의 의존성 증 spring boot 3.5.13 버전과 호환되지 않는 버전을 호환되는 버전으로 변경
    - 공통모듈의 spring cloud 버전을 spring boot 3.5.13 버전과 호환되는 버전으로 다운그레이트
    
    * remove: application.properties 파일 삭제
    
    - 개별 도메인 서비스를 eureka server와 연동 시 개별 도메인 서비스명을 application.properties 파일의 설정이 덮어쓰는 문제가 발생하여 삭제

--- src/main/java/org/pgsg/config/security/LoginFilter.java
+++ src/main/java/org/pgsg/config/security/LoginFilter.java
@@ -27,16 +27,17 @@ public class LoginFilter extends OncePerRequestFilter {
 
     private static final String HEADER_USER_ID = "X-User-Id"; // UUID 고유 사용자 id
     private static final String HEADER_USERNAME = "X-User-Username"; // 로그인 ID로 사용
-    private static final String HEADER_USER_NAME = "X-User-Name";  // 실명
     private static final String HEADER_ROLES = "X-User-Roles";    // 권한
+    private static final String HEADER_USER_NAME = "X-User-Name";  // 실명
+    private static final String HEADER_USER_NICKNAME = "X-User-Nickname";  // 닉네임
     private static final String HEADER_ENABLED = "X-User-Enabled";
-
 
     private final HandlerExceptionResolver resolver;
 
-    public  LoginFilter(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+    public LoginFilter(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.resolver = resolver;
     }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
@@ -69,7 +70,8 @@ public class LoginFilter extends OncePerRequestFilter {
         try {
             UUID uuid = UUID.fromString(userIdHeader);
             String username = usernameHeader.trim();
-            String name = decodeHeader(request.getHeader(HEADER_USER_NAME));
+            String name = decodeHeader(request.getHeader(HEADER_USER_NAME));    // UTF-8 디코딩
+            String nickname = decodeHeader(request.getHeader(HEADER_USER_NICKNAME));
             String roles = request.getHeader(HEADER_ROLES);
             String enabledStr = request.getHeader(HEADER_ENABLED);
 
@@ -79,6 +81,7 @@ public class LoginFilter extends OncePerRequestFilter {
                 .password("")
                 .userRole(roles)
                 .name(name)
+                .nickname(nickname)
                 .enabled("true".equalsIgnoreCase(enabledStr))
                 .build();
 
