ab81c4371bdeebea6fbb87908c53a006663e6066
jin <yjj1544@gmail.com>
2026-04-30 15:34:16 +0900

    refactor: 코드래빗 피드백 반영 5 - OutboxRepository: @Modifying, @Query 추가로 - OutboxService: saveEvent 내 도메인 저장 추가, 직렬화 과정에서 발생하는 예외처리만 따로 처리하도록 수정

--- src/main/java/org/pgsg/common/domain/OutboxRepository.java
+++ src/main/java/org/pgsg/common/domain/OutboxRepository.java
@@ -1,6 +1,8 @@
 package org.pgsg.common.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
@@ -11,6 +13,9 @@ public interface OutboxRepository extends JpaRepository<Outbox, UUID>, QuerydslP
 	List<Outbox> findByStatus(OutboxStatus status);
 	Optional<Outbox> findByCorrelationId(UUID correlationId);
 
+	@Modifying
+	@Query("UPDATE Outbox o SET o.status = :outboxStatus " +
+		"WHERE o.id = :id AND o.status = org.pgsg.common.domain.OutboxStatus.PENDING")
 	int updateStatusIfReady(UUID correlationId, OutboxStatus outboxStatus);
 
 	List<Outbox> findAllByCorrelationId(UUID uuid);
